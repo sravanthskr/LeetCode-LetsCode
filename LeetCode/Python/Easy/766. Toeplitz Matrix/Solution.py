fd7c260e3784fef5ef31ee97500479083e256941
Sravanth Kumar <118090200+sravanthskr@users.noreply.github.com>
2026-09-08 16:40:23 +0530

    Add Easy Python Solution for 766. Toeplitz Matrix from LeetCode

--- LeetCode/Python/Easy/766. Toeplitz Matrix/Solution.py
+++ LeetCode/Python/Easy/766. Toeplitz Matrix/Solution.py
@@ -1,7 +1,8 @@
 class Solution:
     def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
+        found=False
         for i in range(len(matrix)-1):
-            
+    
             for j in range(len(matrix[0])-1):
                 if matrix[i][j]==matrix[i+1][j+1]:
                     found=True
