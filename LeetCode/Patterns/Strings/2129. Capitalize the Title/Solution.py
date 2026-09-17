a6ef8e71abb6b7074bd7664c6d6fe16a3f296587
Sravanth Kumar <118090200+sravanthskr@users.noreply.github.com>
2026-09-17 12:53:52 +0530

    Add Easy Python Solution for 2129. Capitalize the Title from LeetCode

---
+++ LeetCode/Patterns/Strings/2129. Capitalize the Title/Solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def capitalizeTitle(self, title: str) -> str:
+        lst=title.split(" ")
+        for i in range(len(lst)):
+            if len(lst[i])<=2:
+                lst[i]=lst[i].lower()
+            else:
+                lst[i]=lst[i].title()
+        return " ".join(lst)
